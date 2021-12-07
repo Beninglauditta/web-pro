c87a1359f2132b259d13ba9a3ecf11e7885b5b87
beninglauditta <beninglaudittair@gmail.com>
2021-12-07 15:11:35 +0700

    variable tipe data

---
+++ variable-tipedata/index.js
@@ -0,0 +1,23 @@
+// OPERATIONS MATH
+
+// var x = 1;
+// var y = true;
+
+// var total = x + y;
+// console.log(total);
+// typeof utk cek tipe data
+
+// var kata = "sarapan";
+// var angka = 10;
+
+// var result = kata - angka;
+// console.log(typeof result);
+// NaN => Not a Number
+
+// OPERATIONS LOGIC
+
+var a = 10;
+var b = '10';
+
+var c = a === b;
+console.log(c);
