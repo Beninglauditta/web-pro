6371bb39e182b530c484c5da1bc04fb8ef085e9d
beninglauditta <beninglaudittair@gmail.com>
2021-12-07 15:13:24 +0700

    algoritma

---
+++ algoritma/index.js
@@ -0,0 +1,70 @@
+/**
+ * STUDY CASE 1
+ * 
+ * Buatlah sebuah rumus luas segitiga
+ * 
+ * ALGORITMA
+ * 1. Cari definisi dari rumus luas segitiga = 1/2 * alas * tinggi
+ * 2. Buat variable alas
+ * 3. Buat variable tinggi
+ * 4. Buat variable luas
+ * 5. Masukkan rumus luas segitiga ke dalam variable luas
+ * 6. Proses luas
+ * 7. Cetak luas
+ * 
+ * PSEUDOCODE
+ * 
+ * STORE alas WITH ANY NUMBER
+ * STORE tinggi WITH ANY NUMBER
+ * 
+ * SET luas WITH 1/2 MULTIPLY alas MULTIPLY tinggi
+ * PRINT luas
+ */
+
+// var alas = 5;
+// var tinggi = 5;
+
+// var luas = 1 / 2 * alas * tinggi;
+// console.log(luas);
+
+/**
+ * STUDY CASE 2
+ * 
+ * Buatlah luas lingkaran
+ * 
+ * ALGORITMA
+ * 1. Cari definisi dari luas lingkaran = pi * r * r
+ * 2. Buat variable r
+ * 3. Buat variable pi isi dengan angka 3.14
+ * 4. Buat variable luas
+ * 5. Isi luas dengan rumus = pi * r * r
+ * 6. Proses luas
+ * 7. Cetak luas
+ * 
+ * PSEUDOCODE
+ * 
+ * STORE pi WITH 3.14
+ * STORE r WITH ANY NUMBER
+ * 
+ * SET luas WITH pi MULTIPLY r MULTIPLY r
+ * PRINT luas
+ * 
+ */
+
+ var pi = 3.14;
+ var r = 7;
+ 
+ var luas = pi * r * r;
+ console.log(Math.round(luas));
+ // Math.round(luas)
+ 
+ /**
+  * STUDY CASE 3
+  * 
+  * Buat rumus luas permukaan balok
+  * 
+  * ALGORITMA
+  * 
+  * PSEUDOCODE
+  * 
+  */
